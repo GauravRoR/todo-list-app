5845d24f64655db421ca47ca74193a746c63a7e8
GauravRoR <gauravdevock@gmail.com>
2025-04-22 01:22:05 +0530

    corsfix

--- server/server.js
+++ server/server.js
@@ -10,7 +10,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://todo-list-app-ah53.onrender.com', 'http://localhost:3000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
